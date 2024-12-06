573e62fca17f8f75a5966678c4184efcd10c84ec
Hesam <Hesam@HDSR>
2024-12-06 03:53:54 +0330

    Add project files.

---
+++ my first application/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int age = 33;
+const string wellcomeMessage = "Wellcome to C#";
+Console.WriteLine("My name is hesam Age= "+age);
+Console.WriteLine(wellcomeMessage);
+Console.WriteLine();
